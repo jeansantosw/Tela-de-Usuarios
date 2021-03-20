7d4087e826c109bb326d0e408a784ba43a617931
Jean Santos <jeansantos.sw@gmail.com>
2021-03-20 18:08:08 +0000

    user screen

--- index.js
+++ index.js
@@ -1,24 +1,21 @@
-
-var nome = document.querySelector("#exampleInputName");
-var gender = document.querySelectorAll("#form-user-create [name=gender]:checked");
-var birth = document.querySelector("ExemploInputBirth");
-var country = document.querySelector("#ExemploInputCountry");
-var email = document.querySelector("#ExemploInputEmail");
-var password = document.querySelector("#ExemploInputPassword");
-var photo = document.querySelector("#ExemploInputFile");
-var admin = document.querySelector("#exampleInputAdmin");
+var fields = document.querySelectorAll("#form-user-create [name]");
+var user = {}
 
 
+ document.getElementById("form-user-create").addEventListener("submit", function(event){
 
- var fields = document.querySelectorAll("#form-user-create [name]");
+    event.preventDefault();
+    fields.forEach(function (field, index){
 
- fields.forEach(function (field, index){
-
-    if (field.name == "gender") {
-        if(field.checked) console.log("Sim: ", field);
-    } else {
-
-        console.log("Não")
-    }
-
+        if (field.name == "gender") {
+    
+            if(field.checked){
+                user[field.name] = field.value;
+            }
+    
+        } else {
+            user[field.name] = field.value;
+        }
+     });
+     console.log(user);
  });
