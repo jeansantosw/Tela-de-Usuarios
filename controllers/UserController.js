9e1910991e44350242dccd83a1866dff966cdd12
Jean Santos <jeansantos.sw@gmail.com>
2021-03-21 12:13:31 +0000

    user screen

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -13,12 +13,40 @@ class UserController {
         this.formEl.addEventListener("submit", event  =>{
             event.preventDefault();
 
-            
-            this.addLine(this.getValues());
-            
+            let velues = this.getValues();
+          
+            this.getPhoto((content) =>{
+
+                velues.photo = content;
+                this.addLine(velues);
+
+
+            });   
         });
 
     }
+
+    getPhoto(callback) {
+        let fileReader = new FileReader();
+
+        let elements = [...this.formEl.elements].filter(item => {
+            if(item.name === "photo"){
+                return item;
+            }
+        });
+
+        let file = elements[0].files[0];
+
+        fileReader.onload = () => {
+
+            callback(fileReader.result);
+
+        };
+
+        fileReader.readAsDataURL(file);
+    }
+
+
 
     getValues() {
         let user = {};
@@ -56,7 +84,7 @@ class UserController {
         this.tableEl.innerHTML = `
         
         <tr>
-        <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+        <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
         <td>${dataUser.name}</td>
         <td>${dataUser.email}</td>
         <td>${dataUser.admin}</td>
