67fcba3412071020184fdaee614ba84cbba6b7e0
Jean Santos <jeansantos.sw@gmail.com>
2021-04-12 12:18:08 +0100

    tela usuarios

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,97 +1,97 @@
 class UserController {
 
-    constructor(formId, tableId) {
+  constructor(formId, tableId) {
 
-        this.formEl = document.getElementById(formId);
-        this.tableEl = document.getElementById(tableId);
-        this.onSubmit();
+    this.formEl = document.getElementById(formId);
+    this.tableEl = document.getElementById(tableId);
+    this.onSubmit();
 
-    }
- // Enviando formulario. 
-    onSubmit() {
+  }
+  // Enviando formulario. 
+  onSubmit() {
 
-        this.formEl.addEventListener("submit", event => {
-            event.preventDefault();
+    this.formEl.addEventListener("submit", event => {
+      event.preventDefault();
 
-            let values = this.getValues();
+      let values = this.getValues();
 
-            this.getPhoto().then(
-                (content)=> {
-                    values.photo = content;
-                    this.addLine(values);
-            },
-                (e) => {
-                    console.error(e);
-            }
-            );
-        });
+      this.getPhoto().then(
+        (content) => {
+          values.photo = content;
+          this.addLine(values);
+        },
+        (e) => {
+          console.error(e);
+        }
+      );
+    });
 
-    }
-    getPhoto() {
-        return new Promise((resolve, reject) => {
-            let fileReader = new FileReader();
+  }
+  getPhoto() {
+    return new Promise((resolve, reject) => {
+      let fileReader = new FileReader();
 
-            let elements = [...this.formEl.elements].filter(item => {
-                if (item.name === "photo") {
-                    return item;
-                }
-            });
+      let elements = [...this.formEl.elements].filter(item => {
+        if (item.name === "photo") {
+          return item;
+        }
+      });
 
-            let file = elements[0].files[0];
+      let file = elements[0].files[0];
 
-            fileReader.onload = () => {
+      fileReader.onload = () => {
 
-                resolve(fileReader.result);
+        resolve(fileReader.result);
 
-            };
+      };
 
-            fileReader.onerror = (e) => {
-                reject(e);
-            }
+      fileReader.onerror = (e) => {
+        reject(e);
+      }
 
-           if(file){
-            fileReader.readAsDataURL(file);
-           }else{
-               resolve('dist/img/boxed-bg.jpg');
-           }
+      if (file) {
+        fileReader.readAsDataURL(file);
+      } else {
+        resolve('dist/img/boxed-bg.jpg');
+      }
 
-        });
-    }
+    });
+  }
 
-    getValues() {
-        let user = {};
+  getValues() {
+    let user = {};
 
-        [...this.formEl.elements].forEach(function (field, index) {
+    [...this.formEl.elements].forEach(function (field, index) {
 
-            if (field.name == "gender") {
+      if (field.name == "gender") {
 
-                if (field.checked) {
-                    user[field.name] = field.value;
-                }
+        if (field.checked) {
+          user[field.name] = field.value;
+        }
 
-            } else {
-                user[field.name] = field.value;
-            }
-        });
+      } else {
+        user[field.name] = field.value;
+      }
+    });
 
-        return new User(user.name,
-            user.gender,
-            user.birth,
-            user.country,
-            user.email,
-            user.password,
-            user.photo,
-            user.admin
-        );
+    return new User(user.name,
+      user.gender,
+      user.birth,
+      user.country,
+      user.email,
+      user.password,
+      user.photo,
+      user.admin
+    );
 
-    }
+  }
 
-    addLine(dataUser) {
+  addLine(dataUser) {
 
-        console.log(dataUser);
+    console.log(dataUser);
 
 
-        this.tableEl.innerHTML = `
+    this.tableEl.innerHTML = `
         
         <tr>
         <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
@@ -107,7 +107,7 @@ class UserController {
         
         `;
 
-    }
+  }
 
 
 }
